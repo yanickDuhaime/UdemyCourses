36e8fe5219ec19225ca5b8901bec4a958b341025
Yanick Duhaime <1559399@cegepmontpetit.ca>
2022-06-03 10:31:48 -0400

    Started StateMachine

---
+++ Unity3D/UnityThirdPerson1/Assets/Scripts/StateMachines/StateMachine.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using UnityEngine;
+
+public abstract class StateMachine : MonoBehaviour
+{
+    private State currentState;
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState?.Tick(Time.deltaTime);
+    }
+
+    public void SwitchState(State newState)
+    {
+        currentState?.Exit();
+        currentState = newState;
+        currentState?.Enter();
+    }
+}
