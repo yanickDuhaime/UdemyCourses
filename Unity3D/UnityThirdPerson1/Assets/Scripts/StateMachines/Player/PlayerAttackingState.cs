b27761cf72df1a21ccddad171914c1ca1782c655
Yanick Duhaime <1559399@cegepmontpetit.ca>
2022-10-05 16:22:18 -0400

    idk

--- Unity3D/UnityThirdPerson1/Assets/Scripts/StateMachines/Player/PlayerAttackingState.cs
+++ Unity3D/UnityThirdPerson1/Assets/Scripts/StateMachines/Player/PlayerAttackingState.cs
@@ -51,9 +51,6 @@ public class PlayerAttackingState : PlayerBaseState
             }
         }
         
-        
-       
-
         previousFrameTime = normalizedTime;
     }
 
--- Unity3D/UnityThirdPerson1/Assets/Scripts/StateMachines/StateMachine.cs
+++ Unity3D/UnityThirdPerson1/Assets/Scripts/StateMachines/StateMachine.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens;
 using UnityEngine;
 
 public abstract class StateMachine : MonoBehaviour
